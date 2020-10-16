636ce138d065c489c4a1d354f1b63418b01053e9
David Gould <davidrichardgould93@gmail.com>
2020-10-16 17:34:37 -0500

    fall cleaning in the directory

--- database.sql
+++ database.sql
@@ -1,10 +1,40 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+"id" SERIAL PRIMARY KEY,
+"username" VARCHAR (80) UNIQUE NOT NULL,
+"password" VARCHAR (1000) NOT NULL,
+"first_name" VARCHAR (80) NOT NULL,
+"last_name" VARCHAR (80) NOT NULL
+);
+
+CREATE TABLE "address" (
+"id" SERIAL PRIMARY KEY,
+"street" VARCHAR (80) NOT NULL,
+"city" VARCHAR (80) NOT NULL,
+"state" VARCHAR (2) NOT NULL,
+"zip" INT NOT NULL,
+"user_id" INT REFERENCES "user"
+);
+
+CREATE TABLE "pet" (
+"id" SERIAL PRIMARY KEY,
+"name" VARCHAR (80) NOT NULL,
+"age" INT NOT NULL,
+"size" VARCHAR (30) NOT NULL,
+"image_url" VARCHAR (500),
+"user_id" INT REFERENCES "user",
+"address_id" INT REFERENCES "address"
+);
+
+CREATE TABLE "walk" (
+"id" SERIAL PRIMARY KEY,
+"time" DATE NOT NULL,
+"pet_id" INT REFERENCES "pet",
+"address_id" INT REFERENCES "address"
+);
+
+CREATE TABLE "grooming" (
+"id" SERIAL PRIMARY KEY,
+"time" DATE NOT NULL,
+"pet_id" INT REFERENCES "pet",
+"drop_off_address" VARCHAR (300) DEFAULT '101 Doggo ln Blaine, MN 55449'
 );
